f63ddeeb07790d18f3dbea14353f06682fe216ab
Bastien Migette <bmigette@paloaltonetworks.com>
2022-03-31 14:37:02 +0200

    Changed desc

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='''Prisma Access IP API''',  # Required
+    description='''Prisma Access IP API Client''',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
